be708e95e266901db8283f8262659ece52a27488
marydCodes <40235658+marydCodes@users.noreply.github.com>
2021-07-25 17:26:59 +0100

    completed up to Stage 3: simplified Markov chain model

--- Text Generator/task/text_generator/text_generator.py
+++ Text Generator/task/text_generator/text_generator.py
@@ -2,31 +2,41 @@
 
 import nltk
 from nltk.tokenize import WhitespaceTokenizer
+import collections
 # nltk.download("punkt")
 
-# filename = input()
-filename = "corpus.txt"
+filename = input()
+# filename = "corpus.txt"
 f = open(filename, "r", encoding="utf-8")
 corpus = f.read()
 # print(corpus[0:2])
 
+#tokenize corpus
 tkns = WhitespaceTokenizer().tokenize(corpus)
 # print(tkns[0:5])
 
+# create list of bigrams
 bigrams = list(nltk.bigrams(tkns))
 print("Number of bigrams:", len(bigrams))
-print(bigrams[0:6])
 
-while True:
-    in_ = input()
-    try:
-        idx = int(in_)
-        print("Head:", bigrams[idx][0], "Tail:", bigrams[idx][1])
-    except ValueError:
-        if in_ == "exit":
-            False
-            break
-        else:
-            print("Type Error. Please input an integer.")
-    except IndexError:
-        print("Index Error. Please put input a value that is not greater than the number of all bigrams.")
+# dictionary of bigrams
+bgr_dict = collections.defaultdict(list)
+# print(bgr_dict)
+for trn in bigrams:
+    bgr_dict[trn[0]].append(trn[1])
+# print(bgr_dict)
+
+head = '0'
+while head != 'exit':
+    head = input()
+    if head == 'exit':
+        break
+    elif head not in bgr_dict:
+        print(f"Head: {head}")
+        print("Key Error. The requested word is not in the model. Please input another word.")
+    else:
+        print(f"Head: {head}")
+        frq_dict = collections.Counter(bgr_dict[head])
+        most_comn = frq_dict.most_common()
+        for i in most_comn:
+            print(f"Tail: {i[0]}\tCount: {i[1]}")
